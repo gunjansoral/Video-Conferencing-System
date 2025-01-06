8d2b201140da158c0e024c4e7ade1d665a6bedac
gunjan <gunjan.soral@gmail.com>
2025-01-06 08:41:04 +0530

    cors config changed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173', // React app ka URL
+        origin: '*', // React app ka URL
+        // origin: 'http://localhost:5173', // React app ka URL
         methods: ['GET', 'POST']
     }
 });
