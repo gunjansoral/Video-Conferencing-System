b20953c59dafa68d93b3989320eb902c2038d13d
gunjan <gunjan.soral@gmail.com>
2025-01-06 08:54:30 +0530

    backend production url added

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:5000'); // Backend URL
+const socket = io('https://video-conferencing-system.onrender.com'); // Backend URL
 export default socket;
